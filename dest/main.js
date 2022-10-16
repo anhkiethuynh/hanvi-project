efdb103cb54332156919183e24ba2efff3995b3b
kiet.huynh <kiet.huynh@kyanon.digital>
2022-10-16 20:41:27 +0700

    update style and scroll trigger

--- dest/main.js
+++ dest/main.js
@@ -3,58 +3,82 @@ gsap.registerPlugin(ScrollTrigger);
 
 /////////////////////////// SCROLL ///////////////////////////////
 $(document).ready(function () {
-	let header = $(".header"),
-		mobile,
-		tablet;
+  let header = $(".header"),
+    mobile,
+    tablet;
 
-	// DETECT DEVICE
-	// let md = new MobileDetect(window.navigator.userAgent);
-	// function mobileDetect() {
-	//     if (md.mobile() != null || md.tablet() != null) {
-	//         mobile = true
-	//         tablet = true
-	//     } else {
-	//         mobile = false
-	//         tablet = false
-	//     }
-	// }
-	// mobileDetect();
-	function cloneMenu() {
-		const currentMenu = $(".header .menu .menu-wrap");
-		if (currentMenu?.length > 0) {
-			const cloneMenu = currentMenu.clone();
-			const nav = $(".mobile-nav .inner");
+  // DETECT DEVICE
+  // let md = new MobileDetect(window.navigator.userAgent);
+  // function mobileDetect() {
+  //     if (md.mobile() != null || md.tablet() != null) {
+  //         mobile = true
+  //         tablet = true
+  //     } else {
+  //         mobile = false
+  //         tablet = false
+  //     }
+  // }
+  // mobileDetect();
+  function cloneMenu() {
+    const currentMenu = $(".header .menu .menu-wrap");
+    if (currentMenu?.length > 0) {
+      const cloneMenu = currentMenu.clone();
+      const nav = $(".mobile-nav .inner");
 
-			nav.append(cloneMenu);
-		}
-	}
-	function menuMobile() {
-		const nav = $(".mobile-nav"),
-			btnMenu = $(".header .hamburger"),
-			btnClose = $(".mobile-nav .btn-close-nav");
-		btnMenu.on("click", function () {
-			nav.addClass("show");
-		});
-		btnClose.on("click", function () {
-			nav.removeClass("show");
-		});
-	}
-	function init() {
-		$("body")
-			.imagesLoaded()
-			.progress({ background: true }, function (instance, image) {})
-			.always(function (instance) {
-				cloneMenu();
-				menuMobile();
-				$(".loading").addClass("--hide");
-			})
-			.fail(function () {
-				// console.log('all images loaded, at least one is broken');
-			})
-			.done(function (instance) {
-				// console.log('all images successfully loaded');
-			});
-	}
-	init();
+      nav.append(cloneMenu);
+    }
+  }
+  function menuMobile() {
+    const nav = $(".mobile-nav"),
+      btnMenu = $(".header .hamburger"),
+      btnClose = $(".mobile-nav .btn-close-nav");
+    btnMenu.on("click", function () {
+      nav.addClass("show");
+    });
+    btnClose.on("click", function () {
+      nav.removeClass("show");
+    });
+  }
+  function handleScrollToSection() {
+    const actionsToScroll = $("[data-scroll-to]");
+
+    if (!actionsToScroll?.length) return;
+    actionsToScroll.on("click", function (e) {
+      e.preventDefault();
+      $(
+        `[data-target-scroll=${$(this).attr("data-scroll-to")}]`
+      )[0]?.scrollIntoView({
+        behavior: "smooth",
+      });
+    });
+  }
+  function handlePageScroll() {
+    $(window).on("scroll", function () {
+      const header = $(".header");
+      if ($(window).scrollTop() > header.height()) {
+        $(".fixed-navigator").removeClass("hide");
+      } else {
+        $(".fixed-navigator").addClass("hide");
+      }
+    });
+  }
+  function init() {
+    $("body")
+      .imagesLoaded()
+      .progress({ background: true }, function (instance, image) {})
+      .always(function (instance) {
+        cloneMenu();
+        menuMobile();
+        handleScrollToSection();
+        handlePageScroll();
+        $(".loading").addClass("--hide");
+      })
+      .fail(function () {
+        // console.log('all images loaded, at least one is broken');
+      })
+      .done(function (instance) {
+        // console.log('all images successfully loaded');
+      });
+  }
+  init();
 });
-
